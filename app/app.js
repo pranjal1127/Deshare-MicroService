7ea0456a7b87f02a21f528e972dafaa665f886d0
pranjal <2018kucp1127@iiitkota.ac.in>
2022-12-03 15:30:25 +0530

    Add mongoURI in const

--- app/app.js
+++ app/app.js
@@ -2,12 +2,13 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const {MONGO_DB_URL} = require('./config/constant');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-mongoose.connect('mongodb+srv://admin:mGVGXKIVvbly2C9e@cluster0.vz5ifqw.mongodb.net/deshare?retryWrites=true&w=majority')
+mongoose.connect(MONGO_DB_URL)
   .then(() => console.log('connection succesful'))
   .catch((err) => console.error(err));
 
--- app/config/constant.js
+++ app/config/constant.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_DB_URL: "",
+    MONGO_DB_URL: "'mongodb+srv://admin:mGVGXKIVvbly2C9e@cluster0.vz5ifqw.mongodb.net/deshare?retryWrites=true&w=majority'",
     INCENTIVE_MANAGER_CONTRACT_ADDRESS: "0x0Ef95B3afd42a2524Bbd4EBBac5e39191DC48dCe",
     ALCHAMY_RPC_URL: "https://polygon-mumbai.g.alchemy.com/v2/wvnk1t6SNMkWMMFXLxdEAklClJ_IfNXD",
     ADMIN_PRIVATE_KEY: "0x7f1fde717636607bb803505275410e091b79fdd5222485cac60b4966ba274869",
